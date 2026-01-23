79f54e86833e5b36bf5ea7ce13e14caa1472e850
anajuliafl-dev <aj.anajuliafl@gmail.com>
2026-01-23 20:59:18 -0300

    atualização

--- nível-de-herói.js
+++ nível-de-herói.js
@@ -1,2 +1,26 @@
-let nome = "Ana Júlia"
-console.log(nome)
+let nickname = "Tairn"
+let experiencia = 7562
+let nivel = ""
+
+for (let i = 1; i <= 3; i++){
+
+if(experiencia <= 1000){
+  nivel = "ferro"
+}else if(experiencia >= 1001 && experiencia <= 2000){
+  nivel = "bronze";
+}else if(experiencia >= 2001 && experiencia <= 5000){
+  nivel = "prata";
+}else if(experiencia >= 5001 && experiencia <= 7000){
+  nivel = "ouro";
+}else if(experiencia >= 7001 && experiencia <= 8000){
+  nivel = "platina";
+}else if(experiencia >= 8001 && experiencia <= 9000){
+  nivel = "ascendente"
+}else if(experiencia >= 9001 && experiencia <= 10000){
+  nivel = "imortal"
+}else {
+  nivel = "radiante"
+}
+
+console.log("O herói de nome " + nickname + " está no nível " + nivel)
+}
